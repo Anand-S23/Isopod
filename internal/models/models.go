5a4299472df884b6e18823af4289e56b4a593e3e
Singh <singan@centennialsd.org>
2026-04-21 08:28:58 -0400

    Implement NewUser function from NewUserDto

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,4 +1,18 @@
 package models
+ 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+	
+type NewUserDto struct {
+	GitHubID    int64  `json:"github_id"`
+	Username    string `json:"username"`
+	Email       string `json:"email"`
+	AvatarURL   string `json:"avatar_url"`
+	AccessToken string `json:"access_token"`
+}
 
 type User struct {
     ID          string    `json:"id"`
@@ -9,4 +23,17 @@ type User struct {
     AccessToken string    `json:"-"`
     CreatedAt   time.Time `json:"created_at"`
     LastLoginAt time.Time `json:"last_login"`
+}
+
+func NewUser(dto NewUserDto) User {
+	return User{
+		ID:          uuid.New().String(),
+		GitHubID:    dto.GitHubID,
+		Username:    dto.Username,
+		Email:       dto.Email,
+		AvatarURL:   dto.AvatarURL,
+		AccessToken: dto.AccessToken,
+		CreatedAt:   time.Now(),
+		LastLoginAt: time.Now(),
+	}
 }
